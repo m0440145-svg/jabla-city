0dce1bd7daa22e187401aebca4d7ef306a3073f1
m0440145-svg <m0440145@gmail.com>
2026-04-11 17:16:08 +0300

    Delete index.tsx

--- index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App' // تأكد أن الاسم هنا يطابق ملف الكود الرئيسي
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
